f0683381ae978c08efdbf1e0de76c0c05162b691
Ali Ostovar <alee.stvr96>
2023-07-04 02:33:23 +0330

    initial commit

---
+++ utils.py
@@ -0,0 +1,12 @@
+def map_feature(X1, X2):
+    """
+    Feature mapping function to polynomial features    
+    """
+    X1 = np.atleast_1d(X1)
+    X2 = np.atleast_1d(X2)
+    degree = 6
+    out = []
+    for i in range(1, degree+1):
+        for j in range(i + 1):
+            out.append((X1**(i-j) * (X2**j)))
+    return np.stack(out, axis=1)
